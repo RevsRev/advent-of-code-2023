4ae6d629d35f307d60cedb1004accc0735268cbf
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-03 12:46:35 +0000

    Added loading resources in abstract AocSolution class

--- src/main/java/rev/aoc/AocSolution.java
+++ src/main/java/rev/aoc/AocSolution.java
@@ -1,12 +1,48 @@
 package rev.aoc;
 
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public abstract class AocSolution
 {
-    private static final String RESOURCES_PATH = "src/main/resources/aoc";
+    private static final String AOC_RESOURCES_PATH = "/aoc";
 
-    public abstract void solve();
+    public abstract void solve() throws Exception;
 
     public abstract List<String> getResources();
+
+    public Map<String,List<String>> loadResources() throws IOException
+    {
+        Map<String,List<String>> resources = new HashMap<>();
+        Iterator<String> it = getResources().iterator();
+        while (it.hasNext()) {
+            String fileName = it.next();
+            String resourcePath = String.format("%s/%s", AOC_RESOURCES_PATH, fileName);
+            List<String> lines = readLines(resourcePath);
+            resources.put(fileName, lines);
+        }
+        return resources;
+    }
+
+    private List<String> readLines(String resourcePath) throws IOException
+    {
+        List<String> lines = new ArrayList<>();
+
+        InputStream is = null;
+        try {
+            is = getClass().getResourceAsStream(resourcePath);
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader r = new BufferedReader(isr);
+            String line = r.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = r.readLine();
+            }
+            return lines;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
 }
