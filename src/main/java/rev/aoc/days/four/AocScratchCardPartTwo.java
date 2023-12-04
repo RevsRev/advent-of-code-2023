2125041e52f02000ef94e7db69b5bef62e606251
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-04 16:43:25 +0000

    Added unimplemented part two with failing test

---
+++ src/main/java/rev/aoc/days/four/AocScratchCardPartTwo.java
@@ -0,0 +1,17 @@
+package rev.aoc.days.four;
+
+import java.util.Map;
+
+public class AocScratchCardPartTwo extends AocScratchCard
+{
+    public AocScratchCardPartTwo(Iterable<String> resources)
+    {
+        super(resources);
+    }
+
+    @Override
+    protected long solve(Map<Integer, ScratchCard> scratchCards)
+    {
+        return 0;
+    }
+}
---
+++ src/test/java/rev/aoc/days/four/AocScratchCardPartTwoTest.java
@@ -0,0 +1,21 @@
+package rev.aoc.days.four;
+
+import rev.aoc.AocSolution;
+import rev.aoc.AocTest;
+
+import java.util.List;
+
+public class AocScratchCardPartTwoTest extends AocTest<Long>
+{
+    @Override
+    protected Long getExpected()
+    {
+        return (long)30;
+    }
+
+    @Override
+    protected AocSolution<Long> getSolution()
+    {
+        return new AocScratchCardPartTwo(List.of("4.2-scratch-card-test"));
+    }
+}
