6b63b7be4349caa5bf117aee051b9fd8e9111de2
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-05 14:15:37 +0000

    Added class for 5.2 with failing tests

---
+++ src/main/java/rev/aoc/days/d5/SeedAlmanacPartTwo.java
@@ -0,0 +1,17 @@
+package rev.aoc.days.d5;
+
+import rev.aoc.AocSolution;
+
+public class SeedAlmanacPartTwo extends AocSolution<Long>
+{
+    public SeedAlmanacPartTwo(Iterable<String> resources)
+    {
+        super(resources);
+    }
+
+    @Override
+    protected Long solveImpl() throws Exception
+    {
+        return null;
+    }
+}
---
+++ src/test/java/rev/aoc/days/d5/SeedAlmanacPartTwoTest.java
@@ -0,0 +1,21 @@
+package rev.aoc.days.d5;
+
+import rev.aoc.AocSolution;
+import rev.aoc.AocTest;
+
+import java.util.List;
+
+public class SeedAlmanacPartTwoTest extends AocTest<Long>
+{
+    @Override
+    protected Long getExpected()
+    {
+        return (long)0;
+    }
+
+    @Override
+    protected AocSolution<Long> getSolution()
+    {
+        return new SeedAlmanacPartTwo(List.of("5.2-seed-almanac-test"));
+    }
+}
