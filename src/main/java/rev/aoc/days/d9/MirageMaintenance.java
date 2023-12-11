390887c11051a5cbf72dd9b017c28a746024eb54
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-11 18:42:44 +0000

    Added skeleton class for 9.1 with failing test

---
+++ src/main/java/rev/aoc/days/d9/MirageMaintenance.java
@@ -0,0 +1,17 @@
+package rev.aoc.days.d9;
+
+import rev.aoc.AocSolution;
+
+public class MirageMaintenance extends AocSolution<Long>
+{
+    public MirageMaintenance(Iterable<String> resources)
+    {
+        super(resources);
+    }
+
+    @Override
+    protected Long solveImpl() throws Exception
+    {
+        return null;
+    }
+}
---
+++ src/test/java/rev/aoc/days/d9/MirageMaintenanceTest.java
@@ -0,0 +1,21 @@
+package rev.aoc.days.d9;
+
+import rev.aoc.AocSolution;
+import rev.aoc.AocTest;
+
+import java.util.List;
+
+public class MirageMaintenanceTest extends AocTest<Long>
+{
+    @Override
+    protected Long getExpected()
+    {
+        return 114l;
+    }
+
+    @Override
+    protected AocSolution<Long> getSolution()
+    {
+        return new MirageMaintenance(List.of("9.1-mirage-maintenance"));
+    }
+}
