cb3ff03eb9c07908c2a6d0ddcb261c24084215d2
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-12 16:33:18 +0000

    Added skeleton class for 11.1 with failing test

---
+++ src/main/java/rev/aoc/days/d11/CosmicExpansion.java
@@ -0,0 +1,17 @@
+package rev.aoc.days.d11;
+
+import rev.aoc.AocSolution;
+
+public class CosmicExpansion extends AocSolution<Long>
+{
+    public CosmicExpansion(Iterable<String> resources)
+    {
+        super(resources);
+    }
+
+    @Override
+    protected Long solveImpl() throws Exception
+    {
+        return null;
+    }
+}
