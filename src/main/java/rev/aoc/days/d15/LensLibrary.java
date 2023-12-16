e91e7b1e15b76d6b5e9d333ccc59fb99661a9ab1
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-16 13:50:32 +0000

    Implemented 15.1 to fix failing test

--- src/main/java/rev/aoc/days/d15/LensLibrary.java
+++ src/main/java/rev/aoc/days/d15/LensLibrary.java
@@ -2,6 +2,8 @@ package rev.aoc.days.d15;
 
 import rev.aoc.AocSolution;
 
+import java.util.List;
+
 public class LensLibrary extends AocSolution<Long>
 {
     public LensLibrary(Iterable<String> resources)
@@ -12,6 +14,36 @@ public class LensLibrary extends AocSolution<Long>
     @Override
     protected Long solveImpl() throws Exception
     {
-        return null;
+        List<String> lines = getOneAndOnlyResourceLines();
+        char[][] inputs = parse(lines);
+
+        long hashTotal = 0;
+        int len = inputs.length;
+        for (int i=0; i<len; i++) {
+            hashTotal+=calHash(inputs[i]);
+        }
+        return hashTotal;
+    }
+
+    private long calHash(char[] input)
+    {
+        long val = 0;
+        int hashMultiplier = 17;
+        int hashMod = 256;
+        for (int i=0; i<input.length; i++) {
+            val = (hashMultiplier *(val+input[i]))% hashMod;
+        }
+        return val;
+    }
+
+    private char[][] parse(List<String> lines)
+    {
+        String line = lines.get(0);
+        String[] inputsStr = line.split(",");
+        char[][] inputs = new char[inputsStr.length][];
+        for (int i=0; i<inputsStr.length;i++) {
+            inputs[i] = inputsStr[i].toCharArray();
+        }
+        return inputs;
     }
 }
