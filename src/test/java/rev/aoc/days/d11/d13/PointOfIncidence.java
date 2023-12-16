bbca7fe1a94efd5e71ef71428f7cdd0597ff0a81
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-16 01:13:36 +0000

    Created skeleton class for 13.1 with failing test

---
+++ src/test/java/rev/aoc/days/d11/d13/PointOfIncidence.java
@@ -0,0 +1,17 @@
+package rev.aoc.days.d11.d13;
+
+import rev.aoc.AocSolution;
+
+public class PointOfIncidence extends AocSolution<Long>
+{
+    public PointOfIncidence(Iterable<String> resources)
+    {
+        super(resources);
+    }
+
+    @Override
+    protected Long solveImpl() throws Exception
+    {
+        return null;
+    }
+}
---
+++ src/test/java/rev/aoc/days/d13/PointOfIncidenceTest.java
@@ -0,0 +1,22 @@
+package rev.aoc.days.d13;
+
+import rev.aoc.AocSolution;
+import rev.aoc.AocTest;
+import rev.aoc.days.d11.d13.PointOfIncidence;
+
+import java.util.List;
+
+public class PointOfIncidenceTest extends AocTest<Long>
+{
+    @Override
+    protected Long getExpected()
+    {
+        return 405l;
+    }
+
+    @Override
+    protected AocSolution<Long> getSolution()
+    {
+        return new PointOfIncidence(List.of("13.1-point-of-incidence-test"));
+    }
+}
