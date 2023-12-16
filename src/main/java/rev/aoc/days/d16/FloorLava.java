3b3036d33bfeab9e9c8c5d7a51530d26c668bd47
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-16 17:14:14 +0000

    Made class for 16.1 with failing test

---
+++ src/main/java/rev/aoc/days/d16/FloorLava.java
@@ -0,0 +1,17 @@
+package rev.aoc.days.d16;
+
+import rev.aoc.AocSolution;
+
+public class FloorLava extends AocSolution<Long>
+{
+    public FloorLava(Iterable<String> resources)
+    {
+        super(resources);
+    }
+
+    @Override
+    protected Long solveImpl() throws Exception
+    {
+        return null;
+    }
+}
