cb13e83d91899a4bd9d8d1bbecbbe3df631f3741
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-18 10:56:16 +0000

    Implemented 17.2, making solution to 17.1 more general as well

--- src/main/java/rev/aoc/days/d17/ClumsyCrucible.java
+++ src/main/java/rev/aoc/days/d17/ClumsyCrucible.java
@@ -1,26 +1,16 @@
 package rev.aoc.days.d17;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
-import org.jgrapht.graph.SimpleWeightedGraph;
 import rev.aoc.AocSolution;
-import rev.aoc.math.vec.Vec2;
 import rev.aoc.math.vec.Vec4;
 
 import java.util.*;
 
 public abstract class ClumsyCrucible extends AocSolution<Long>
 {
-    private static final int UNSET = -2;
-    private static final int INVALID = -1;
-
-    private static final int UP = 0;
-    private static final int RIGHT = 1;
-    private static final int DOWN = 2;
-    private static final int LEFT = 3;
 
     public ClumsyCrucible(Iterable<String> resources)
     {
@@ -38,7 +28,10 @@ public abstract class ClumsyCrucible extends AocSolution<Long>
         SimpleDirectedWeightedGraph<Vec4, DefaultWeightedEdge> graph = toGraph(cityBlock);
 
         long result = Long.MAX_VALUE;
-        for (int k=0; k<=3; k++) {
+        for (int k = -getMaximumRunLength(); k<= getMaximumRunLength(); k++) {
+            if (k>-getMinimumRunLength() && k<getMinimumRunLength()) {
+                continue;
+            }
             GraphPath<Vec4, DefaultWeightedEdge> pathBetween = DijkstraShortestPath.findPathBetween(graph, new Vec4(0, 0, 0, 0), new Vec4(height - 1, width - 1, k, 0));
             if (pathBetween != null) {
                 result = Math.min(result, (long)pathBetween.getWeight());
@@ -51,7 +44,78 @@ public abstract class ClumsyCrucible extends AocSolution<Long>
         return result;
     }
 
-    protected abstract SimpleDirectedWeightedGraph<Vec4, DefaultWeightedEdge> toGraph(int[][] cityBlock);
+    protected abstract int getMaximumRunLength();
+    protected abstract int getMinimumRunLength();
+
+    protected SimpleDirectedWeightedGraph<Vec4, DefaultWeightedEdge> toGraph(int[][] cityBlock)
+    {
+        int height = cityBlock.length;
+        int width = cityBlock[0].length;
+
+        SimpleDirectedWeightedGraph<Vec4, DefaultWeightedEdge> graph = new SimpleDirectedWeightedGraph(DefaultWeightedEdge.class);
+
+        int max = getMaximumRunLength();
+        int min = getMinimumRunLength();
+
+        for (int i = 0; i< height; i++) {
+            for (int j = 0; j< width; j++) {
+                for (int k=-max; k<=max; k++) {
+                    graph.addVertex(new Vec4(i,j,k,0));
+                    graph.addVertex(new Vec4(i,j,0,k));
+                }
+            }
+        }
+
+        for (int i = 0; i< height; i++) {
+            for (int j = 0; j< width; j++) {
+                for (int k=-max; k<=max; k++) {
+
+                    if (i+1< height) {
+                        if (0<=k && k<=max-1) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i+1,j,k+1,0));
+                            graph.setEdgeWeight(edge, cityBlock[i+1][j]);
+                        }
+                        if (Math.abs(k) >= min) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i+1,j,1,0));
+                            graph.setEdgeWeight(edge, cityBlock[i+1][j]);
+                        }
+
+                    }
+                    if (j+1< width) {
+                        if (0<=k && k<=max-1) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i,j+1,0,k+1));
+                            graph.setEdgeWeight(edge, cityBlock[i][j+1]);
+                        }
+                        if (Math.abs(k) >= min) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i,j+1,0,1));
+                            graph.setEdgeWeight(edge, cityBlock[i][j+1]);
+                        }
+                    }
+                    if (i-1>=0) {
+                        if (k<=0 && k>=-max+1) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i-1,j,k-1,0));
+                            graph.setEdgeWeight(edge, cityBlock[i-1][j]);
+                        }
+                        if (Math.abs(k) >= min) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i-1,j,-1,0));
+                            graph.setEdgeWeight(edge, cityBlock[i-1][j]);
+                        }
+                    }
+                    if (j-1>=0) {
+                        if (k<=0 && k>=-max+1) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i,j-1,0,k-1));
+                            graph.setEdgeWeight(edge, cityBlock[i][j-1]);
+                        }
+                        if (Math.abs(k) >= min) {
+                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i,j-1,0,-1));
+                            graph.setEdgeWeight(edge, cityBlock[i][j-1]);
+                        }
+                    }
+                }
+            }
+        }
+        return graph;
+    }
     private int[][] parse(List<String> lines)
     {
         int[][] cityBlock = new int[lines.size()][];
--- src/main/java/rev/aoc/days/d17/ClumsyCruciblePartOne.java
+++ src/main/java/rev/aoc/days/d17/ClumsyCruciblePartOne.java
@@ -12,70 +12,14 @@ public class ClumsyCruciblePartOne extends ClumsyCrucible
     }
 
     @Override
-    protected SimpleDirectedWeightedGraph<Vec4, DefaultWeightedEdge> toGraph(int[][] cityBlock)
+    protected int getMaximumRunLength()
     {
-        int height = cityBlock.length;
-        int width = cityBlock[0].length;
+        return 3;
+    }
 
-        SimpleDirectedWeightedGraph<Vec4, DefaultWeightedEdge> graph = new SimpleDirectedWeightedGraph(DefaultWeightedEdge.class);
-
-        for (int i = 0; i< height; i++) {
-            for (int j = 0; j< width; j++) {
-                for (int k=0; k<=3; k++) {
-                    graph.addVertex(new Vec4(i,j,k,0));
-                    graph.addVertex(new Vec4(i,j,0,k));
-                }
-            }
-        }
-
-        for (int i = 0; i< height; i++) {
-            for (int j = 0; j< width; j++) {
-                for (int k=0; k<=3; k++) {
-
-                    if (i+1< height) {
-                        if (k<=2) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i+1,j,k+1,0));
-                            graph.setEdgeWeight(edge, cityBlock[i+1][j]);
-                        }
-                        if (k != 0) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i+1,j,1,0));
-                            graph.setEdgeWeight(edge, cityBlock[i+1][j]);
-                        }
-
-                    }
-                    if (j+1< width) {
-                        if (k<=2) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i,j+1,0,k+1));
-                            graph.setEdgeWeight(edge, cityBlock[i][j+1]);
-                        }
-                        if (k != 0) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i,j+1,0,1));
-                            graph.setEdgeWeight(edge, cityBlock[i][j+1]);
-                        }
-                    }
-                    if (i-1>=0) {
-                        if (k<=2) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i-1,j,k+1,0));
-                            graph.setEdgeWeight(edge, cityBlock[i-1][j]);
-                        }
-                        if (k!=0) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i-1,j,1,0));
-                            graph.setEdgeWeight(edge, cityBlock[i-1][j]);
-                        }
-                    }
-                    if (j-1>=0) {
-                        if (k<=2) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,0,k), new Vec4(i,j-1,0,k+1));
-                            graph.setEdgeWeight(edge, cityBlock[i][j-1]);
-                        }
-                        if (k!=0) {
-                            DefaultWeightedEdge edge = graph.addEdge(new Vec4(i,j,k,0), new Vec4(i,j-1,0,1));
-                            graph.setEdgeWeight(edge, cityBlock[i][j-1]);
-                        }
-                    }
-                }
-            }
-        }
-        return graph;
+    @Override
+    protected int getMinimumRunLength()
+    {
+        return 1;
     }
 }
