e581b5ad415e887840d806ccea4f0a4d6f1474f7
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-20 23:22:29 +0000

    Added methods for intersecting array ranges in array util

--- src/main/java/rev/aoc/util/ArraysUtil.java
+++ src/main/java/rev/aoc/util/ArraysUtil.java
@@ -78,6 +78,81 @@ public class ArraysUtil {
     return trim(result, comparator, resultIndex);
   }
 
+  /**
+   * Intersects ranges of arrays
+   *
+   * @param first An array of elements of type T, with the format
+   *     [r1Start,r1End,r2Start,r2End,...rNStart,rNEnd] Where rI is a range, start is inclusive and
+   *     end is exclusive, and the elements are already sorted in order. Note that the ranges should
+   *     be non overlapping (rIEnd = rI+1Start is okay, but should be avoided).
+   * @param second A second array in the same format as the first
+   * @param comparator A comparator for comparing type T
+   * @return An intersected range, formed by overlapping ranges from first[] and second[]
+   * @param <T> Type of element being sorted
+   */
+  public static <T> T[] intersectRanges(T[] first, T[] second, Comparator<T> comparator) {
+    if (first.length > second.length) {
+      return intersectRanges(second, first, comparator);
+    }
+
+    if (first.length == 0 || second.length == 0) {
+      return (T[]) new Object[0];
+    }
+
+    T[] result = (T[]) new Object[first.length + second.length];
+    int resultIndex = 0;
+
+    for (int i = 0; i < first.length; i += 2) {
+      T start = first[i];
+      T end = first[i + 1];
+      int startPoint = Arrays.binarySearch(second, start, comparator);
+
+      if (startPoint < 0) {
+        startPoint = -startPoint - 1;
+      }
+
+      if (startPoint % 2 == 0) { // we are after an end and before a start
+        // get all parts of second contained within this region
+        while (startPoint < second.length - 1
+            && comparator.compare(second[startPoint + 1], end) <= 0) {
+          result[resultIndex++] = second[startPoint];
+          result[resultIndex++] = second[startPoint + 1];
+          startPoint += 2;
+        }
+        // add on the next part of second if it overlaps with this region
+        if (startPoint < second.length - 1 && comparator.compare(second[startPoint], end) < 0) {
+          result[resultIndex++] = second[startPoint];
+          result[resultIndex++] = end;
+        }
+      } else { // we are after a start and before an end
+        // get the part of second that overlaps with this region
+        if (comparator.compare(end, second[startPoint]) <= 0) {
+          result[resultIndex++] = start;
+          result[resultIndex++] = end;
+          startPoint += 2;
+        } else {
+          result[resultIndex++] = start;
+          result[resultIndex++] = second[startPoint];
+          startPoint += 2;
+        }
+        // get all parts of second contained within this region
+        while (startPoint < second.length
+            && comparator.compare(second[startPoint - 1], end) <= 0
+            && comparator.compare(second[startPoint], end) <= 0) {
+          result[resultIndex++] = second[startPoint - 1];
+          result[resultIndex++] = second[startPoint];
+          startPoint += 2;
+        }
+        // add on the overlapping end
+        if (startPoint < second.length && comparator.compare(second[startPoint - 1], end) < 0) {
+          result[resultIndex++] = second[startPoint - 1];
+          result[resultIndex++] = end;
+        }
+      }
+    }
+    return trim(result, comparator, resultIndex);
+  }
+
   private static <T> T[] trim(T[] toTrim, Comparator<T> comparator, int toIndex) {
     T[] result = (T[]) new Object[toIndex];
     int index = 0;
--- src/test/java/rev/aoc/util/ArraysUtilTest.java
+++ src/test/java/rev/aoc/util/ArraysUtilTest.java
@@ -33,4 +33,27 @@ public class ArraysUtilTest {
   private <T> void testUnionRanges(T[] expected, T[] a, T[] b, Comparator<T> comparator) {
     Assertions.assertArrayEquals(expected, ArraysUtil.unionRanges(a, b, comparator));
   }
+
+  @Test
+  public void testIntersectRanges() {
+    testIntersectRanges(
+        new Integer[] {2, 4},
+        new Integer[] {1, 4},
+        new Integer[] {2, 6},
+        Comparator.naturalOrder());
+    testIntersectRanges(
+        new Integer[] {2, 3, 7, 9},
+        new Integer[] {2, 9},
+        new Integer[] {1, 3, 7, 10},
+        Comparator.naturalOrder());
+    testIntersectRanges(
+        new Integer[] {2, 3, 7, 9, 15, 17, 19, 23},
+        new Integer[] {2, 9, 15, 17, 19, 28},
+        new Integer[] {1, 3, 7, 23},
+        Comparator.naturalOrder());
+  }
+
+  private <T> void testIntersectRanges(T[] expected, T[] a, T[] b, Comparator<T> comparator) {
+    Assertions.assertArrayEquals(expected, ArraysUtil.intersectRanges(a, b, comparator));
+  }
 }
