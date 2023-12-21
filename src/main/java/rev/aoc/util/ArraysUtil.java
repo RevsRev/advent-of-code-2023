729e78d7fbade053efb1a7f28d3562b190ecdb9a
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-12-21 13:57:29 +0000

    Added complement method to ArraysUtil

--- src/main/java/rev/aoc/util/ArraysUtil.java
+++ src/main/java/rev/aoc/util/ArraysUtil.java
@@ -1,5 +1,6 @@
 package rev.aoc.util;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -17,9 +18,10 @@ public class ArraysUtil {
    * @return A combined range, in the same format as the inputs.
    * @param <T> Type of element being sorted
    */
-  public static <T> T[] unionRanges(T[] first, T[] second, Comparator<T> comparator) {
+  public static <T extends Comparable<? super T>> T[] unionRanges(
+      T[] first, T[] second, Comparator<? super T> comparator, Class<T> clazz) {
     if (first.length > second.length) {
-      return unionRanges(second, first, comparator);
+      return unionRanges(second, first, comparator, clazz);
     }
 
     if (first.length == 0) {
@@ -29,7 +31,7 @@ public class ArraysUtil {
       return first;
     }
 
-    T[] result = (T[]) new Object[first.length + second.length];
+    T[] result = (T[]) Array.newInstance(clazz, first.length + second.length);
     int resultIndex = 0;
 
     for (int i = 1; i < second.length; i += 2) {
@@ -75,7 +77,7 @@ public class ArraysUtil {
       }
     }
 
-    return trim(result, comparator, resultIndex);
+    return trim(result, comparator, resultIndex, clazz);
   }
 
   /**
@@ -90,16 +92,18 @@ public class ArraysUtil {
    * @return An intersected range, formed by overlapping ranges from first[] and second[]
    * @param <T> Type of element being sorted
    */
-  public static <T> T[] intersectRanges(T[] first, T[] second, Comparator<T> comparator) {
+  public static <T extends Comparable<? super T>> T[] intersectRanges(
+      T[] first, T[] second, Comparator<? super T> comparator, Class<T> clazz) {
     if (first.length > second.length) {
-      return intersectRanges(second, first, comparator);
+      return intersectRanges(second, first, comparator, clazz);
     }
 
     if (first.length == 0 || second.length == 0) {
-      return (T[]) new Object[0];
+      return (T[]) Array.newInstance(clazz, 0);
     }
 
-    T[] result = (T[]) new Object[first.length + second.length];
+    T[] result = (T[]) Array.newInstance(clazz, first.length + second.length);
+    ;
     int resultIndex = 0;
 
     for (int i = 0; i < first.length; i += 2) {
@@ -150,11 +154,59 @@ public class ArraysUtil {
         }
       }
     }
-    return trim(result, comparator, resultIndex);
+    return trim(result, comparator, resultIndex, clazz);
   }
 
-  private static <T> T[] trim(T[] toTrim, Comparator<T> comparator, int toIndex) {
-    T[] result = (T[]) new Object[toIndex];
+  /**
+   * Returns the compliment of data in the range {start, end}
+   *
+   * @param data
+   * @param start
+   * @param end
+   * @return
+   * @param <T>
+   */
+  public static <T extends Comparable<? super T>> T[] compliment(
+      T[] data, Comparator<? super T> comparator, T start, T end, Class<T> clazz) {
+    if (data.length == 0) {
+      T[] result = (T[]) Array.newInstance(clazz, 2);
+      result[0] = start;
+      result[1] = end;
+      return result;
+    }
+
+    T[] result = (T[]) Array.newInstance(clazz, data.length + 2);
+    int resultIndex = 0;
+
+    int i = 1;
+    while (i < data.length) {
+      if (i < data.length && comparator.compare(data[i], start) < 0) {
+        i += 2;
+        continue;
+      }
+
+      T endElement = data[i];
+      if (resultIndex == 0 && comparator.compare(data[i - 1], start) > 0) {
+        result[resultIndex++] = start;
+        result[resultIndex++] = data[i - 1];
+      }
+
+      if (i < data.length - 1 && comparator.compare(data[i + 1], end) < 0) {
+        result[resultIndex++] = data[i];
+        result[resultIndex++] = data[i + 1];
+      } else if (comparator.compare(data[i], end) < 0) {
+        result[resultIndex++] = data[i];
+        result[resultIndex++] = end;
+      }
+      i += 2;
+    }
+    return trim(result, comparator, resultIndex, clazz);
+  }
+
+  private static <T extends Comparable<? super T>> T[] trim(
+      T[] toTrim, Comparator<? super T> comparator, int toIndex, Class<T> clazz) {
+    T[] result = (T[]) Array.newInstance(clazz, toIndex);
+    ;
     int index = 0;
     for (int i = 0; i < toIndex - 1; i += 2) {
       T start = toTrim[i];
--- src/test/java/rev/aoc/util/ArraysUtilTest.java
+++ src/test/java/rev/aoc/util/ArraysUtilTest.java
@@ -12,26 +12,31 @@ public class ArraysUtilTest {
         new Integer[] {1, 2, 3, 4, 5, 6},
         new Integer[] {1, 2, 5, 6},
         new Integer[] {3, 4},
-        Comparator.naturalOrder());
+        Comparator.naturalOrder(),
+        Integer.class);
     testUnionRanges(
         new Integer[] {1, 6},
         new Integer[] {1, 3, 4, 6},
         new Integer[] {3, 4},
-        Comparator.naturalOrder());
+        Comparator.naturalOrder(),
+        Integer.class);
     testUnionRanges(
         new Integer[] {1, 7, 8, 10},
         new Integer[] {1, 5, 8, 10},
         new Integer[] {3, 7},
-        Comparator.naturalOrder());
+        Comparator.naturalOrder(),
+        Integer.class);
     testUnionRanges(
         new Integer[] {1, 5, 8, 12},
         new Integer[] {1, 5, 8, 10},
         new Integer[] {10, 12},
-        Comparator.naturalOrder());
+        Comparator.naturalOrder(),
+        Integer.class);
   }
 
-  private <T> void testUnionRanges(T[] expected, T[] a, T[] b, Comparator<T> comparator) {
-    Assertions.assertArrayEquals(expected, ArraysUtil.unionRanges(a, b, comparator));
+  private <T extends Comparable<? super T>> void testUnionRanges(
+      T[] expected, T[] a, T[] b, Comparator<T> comparator, Class<T> clazz) {
+    Assertions.assertArrayEquals(expected, ArraysUtil.unionRanges(a, b, comparator, clazz));
   }
 
   @Test
@@ -40,20 +45,50 @@ public class ArraysUtilTest {
         new Integer[] {2, 4},
         new Integer[] {1, 4},
         new Integer[] {2, 6},
-        Comparator.naturalOrder());
+        Comparator.naturalOrder(),
+        Integer.class);
     testIntersectRanges(
         new Integer[] {2, 3, 7, 9},
         new Integer[] {2, 9},
         new Integer[] {1, 3, 7, 10},
-        Comparator.naturalOrder());
+        Comparator.naturalOrder(),
+        Integer.class);
     testIntersectRanges(
         new Integer[] {2, 3, 7, 9, 15, 17, 19, 23},
         new Integer[] {2, 9, 15, 17, 19, 28},
         new Integer[] {1, 3, 7, 23},
-        Comparator.naturalOrder());
+        Comparator.naturalOrder(),
+        Integer.class);
   }
 
-  private <T> void testIntersectRanges(T[] expected, T[] a, T[] b, Comparator<T> comparator) {
-    Assertions.assertArrayEquals(expected, ArraysUtil.intersectRanges(a, b, comparator));
+  private <T extends Comparable<? super T>> void testIntersectRanges(
+      T[] expected, T[] a, T[] b, Comparator<T> comparator, Class<T> clazz) {
+    Assertions.assertArrayEquals(expected, ArraysUtil.intersectRanges(a, b, comparator, clazz));
+  }
+
+  @Test
+  public void testCompliment() {
+    testCompliment(
+        new Integer[] {0, 2, 7, 10},
+        new Integer[] {2, 7},
+        Comparator.naturalOrder(),
+        0,
+        10,
+        Integer.class);
+    testCompliment(
+        new Integer[] {0, 2}, new Integer[] {2, 7}, Comparator.naturalOrder(), 0, 5, Integer.class);
+    testCompliment(
+        new Integer[] {0, 2, 7, 13, 25, 27, 28, 30},
+        new Integer[] {2, 7, 13, 25, 27, 28},
+        Comparator.naturalOrder(),
+        0,
+        30,
+        Integer.class);
+  }
+
+  private <T extends Comparable<? super T>> void testCompliment(
+      T[] expected, T[] data, Comparator<T> comparator, T start, T end, Class<T> clazz) {
+    Assertions.assertArrayEquals(
+        expected, ArraysUtil.compliment(data, comparator, start, end, clazz));
   }
 }
